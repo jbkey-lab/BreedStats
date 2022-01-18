97b15f09c2be95ef5f1bd575a8f9404f637528e6
Jacob Lamkey <69020457+jbkey730@users.noreply.github.com>
2022-01-18 16:53:55 -0600

    added xgboost model and added grepl in parallel

--- R/xgboostBV.R
+++ R/xgboostBV.R
@@ -35,46 +35,70 @@ xgblinearBV = function(){
   ######################################################
 
 
+  #BV.HSIdentical.df = fread(paste0(hdp,"BV.HSIdentical.df.all.csv"))
   BV.HSIdentical.df = fread(paste0(hdp,"BV.HSIdentical.df.csv"))
+
+  male = trainingx2[!duplicated(trainingx2$MALE), c("MALE",'LINE')]
+  male$num = c(1:(nrow(male)+0))
+
+  female = trainingx2[!duplicated(trainingx2$FEMALE), c("FEMALE",'LINE')]
+  female$num = c(1:(nrow(female)+0))
+
+  field = trainingx2[!duplicated(trainingx2$FIELD), c("FIELD",'LINE')]
+  field$num = c(1:(nrow(field)+0))
+
   BV.HSIdentical.df = BV.HSIdentical.df %>% filter(Plot.Discarded != "Yes",
                                                    Plot.Status != "3 - Bad" )
 
   BV.HSIdentical.df.2 = BV.HSIdentical.df[,c(1:29,31,30,32:33)]
   colnames(BV.HSIdentical.df.2)[c(30:31)] = c("MALE","FEMALE")
 
-  #trainingx2 = data.frame(BV.HSIdentical.df)
+  trainingx2 = data.frame(BV.HSIdentical.df)
 
-  trainingx2 = rbind(BV.HSIdentical.df.2,BV.HSIdentical.df)
-  trainingx2 = data.frame(trainingx2)
+  # trainingx2 = rbind(BV.HSIdentical.df.2,BV.HSIdentical.df)
+  # trainingx2 = data.frame(trainingx2)
 
-
-  field = (trainingx2[!duplicated(trainingx2$FIELD), c("FIELD",'LINE')])
-  field$num = c(1:(nrow(field)+0))
-  trainingx2 = left_join(trainingx2, field[,c(1,3)], by="FIELD")
+  field.2 = (trainingx2[!duplicated(trainingx2$FIELD), c("FIELD",'LINE')])
+  field.2$num = c(1:(nrow(field.2)+0))
+  trainingx2 = left_join(trainingx2, field.2[,c(1,3)], by="FIELD")
   colnames(trainingx2)[34] = "field"
 
-  field = trainingx2[!duplicated(trainingx2$LINE), c("LINE",'MALE')]
-  field$num = c(1:(nrow(field)+0))
-  trainingx2 = left_join(trainingx2, field[,c(1,3)], by="LINE")
+  id = trainingx2[!duplicated(trainingx2$LINE), c("LINE",'MALE')]
+  id$num = c(1:(nrow(id)+0))
+  trainingx2 = left_join(trainingx2, id[,c(1,3)], by="LINE")
   colnames(trainingx2)[35] = "ID"
 
-  field = trainingx2[!duplicated(trainingx2$MALE), c("MALE",'LINE')]
-  field$num = c(1:(nrow(field)+0))
-  trainingx2 = left_join(trainingx2, field[,c(1,3)], by="MALE")
+  male.2 = trainingx2[!duplicated(trainingx2$MALE), c("MALE",'LINE')]
+  male.2$num = c(1:(nrow(male.2)+0))
+  trainingx2 = left_join(trainingx2, male.2[,c(1,3)], by="MALE")
   colnames(trainingx2)[36] = "male"
 
-  field = trainingx2[!duplicated(trainingx2$FEMALE), c("FEMALE",'LINE')]
-  field$num = c(1:(nrow(field)+0))
-  trainingx2 = left_join(trainingx2, field[,c(1,3)], by="FEMALE")
+  female.2 = trainingx2[!duplicated(trainingx2$FEMALE), c("FEMALE",'LINE')]
+  female.2$num = c(1:(nrow(female.2)+0))
+  trainingx2 = left_join(trainingx2, female.2[,c(1,3)], by="FEMALE")
   colnames(trainingx2)[37] = "female"
 
-  field = trainingx2[!duplicated(trainingx2$YEAR), c("YEAR",'LINE')]
-  field$num = c(1:(nrow(field)+0))
-  trainingx2 = left_join(trainingx2, field[,c(1,3)], by="YEAR")
+  Year = trainingx2[!duplicated(trainingx2$YEAR), c("YEAR",'LINE')]
+  Year$num = c(1:(nrow(Year)+0))
+  trainingx2 = left_join(trainingx2, Year[,c(1,3)], by="YEAR")
   colnames(trainingx2)[38] = "Year"
 
+  variety = (trainingx2[!duplicated(trainingx2$Variety), c("Variety",'LINE')])
+  variety$num = c(1:(nrow(variety)+0))
+  trainingx2 = left_join(trainingx2, variety[,c(1,3)], by="Variety")
+  colnames(trainingx2)[39] = "variety"
 
-  trainingx2 = na.omit(trainingx2[,c(22,36,37,35,34,38)]) #yield = 22, plt.height=13
+  #########################
+  testx2 = expand.grid(male$num, female$num, field$num)
+  testx2 = data.frame(testx2, Year=21, ID=paste0(testx2$Var1," + ",testx2$Var2 ))
+  testx2= left_join(testx2, variety[,-1], by=c("ID"="LINE"))
+  colnames(testx2)=c("male","female","field","Year","ID","variety")
+  # field = (trainingx2[!duplicated(trainingx2$Pedigree), c("Pedigree",'LINE')])
+  # field$num = c(1:(nrow(field)+0))
+  # trainingx2 = left_join(trainingx2, field[,c(1,3)], by="Pedigree")
+  # colnames(trainingx2)[40] = "ID"
+
+  trainingx2 = na.omit(trainingx2[,c(22,39,37,35,36,38,34)]) #yield = 22, plt.height=13, ear=10
 
   ID = trainingx2[!duplicated(trainingx2[,4]), 4]
   ID = data.frame(ID)
@@ -107,8 +131,9 @@ xgblinearBV = function(){
   ##################################################################
   final_grid1=expand.grid(nrounds=550, eta=.5, max_depth=3, gamma=0,colsample_bytree=0.95,min_child_weight=1,subsample = 1)
   #final_grid2=expand.grid(nrounds=100, eta=.5, max_depth=5, gamma=0,colsample_bytree=0.95,min_child_weight=1,subsample = 1)
-  #final_grid1 <- expand.grid(nrounds = 500, eta = .3, lambda = .5, alpha=.9)
-  final_grid2 <- expand.grid(nrounds = 550, eta = .5, lambda = .9, alpha=.8)
+
+  #final_grid1 <- expand.grid(nrounds = 500, eta = .3, lambda = .5, alpha=1.5)
+  final_grid2 <- expand.grid(nrounds = 550, eta = .5, lambda = .9, alpha=2)
 
   final_grid3 <- expand.grid(nrounds = 500, eta = .7, lambda = .5, alpha=.9)
   final_grid4 <- expand.grid(nrounds = 550, eta = .9, lambda = .9, alpha=.8)
@@ -118,13 +143,13 @@ xgblinearBV = function(){
 
   models.list2 <- caretList(
     x=trainx2[ , -1],
-    y=(trainx2$Yield),
+    y=(trainx2[,1]),
     continue_on_fail = T,
     trControl=trainControl(method="cv",
                            number=1, #1
-                           index = createFolds((trainx2$Yield),k=2), #2
+                           index = createFolds((trainx2[,1]),k=2), #2
                            savePredictions = TRUE,
-                           classProbs=T,
+                           #classProbs=T,
                            allowParallel = TRUE,
                            verboseIter = TRUE
                            #preProcOptions =  c( method = c("center", "scale"))
@@ -149,14 +174,16 @@ xgblinearBV = function(){
       #  qrf3=caretModelSpec(method="qrf", ntree=10, tuneLength = 1), #9
       # # #qrf4=caretModelSpec(method="qrf", ntree = 150, tuneLength = 1), #7
       #qrf5=caretModelSpec(method="qrf", ntree=10, tuneLength = 1), #5
-      #qrf6=caretModelSpec(method="xgbTree", tuneGrid = final_grid1), #5
+      qrf6=caretModelSpec(method="xgbTree", tuneGrid = final_grid1), #5
+      #qrf5=caretModelSpec(method="xgbLinear", tuneGrid = final_grid1), #5
+      #qrf6=caretModelSpec(method="xgbLinear", tuneGrid = final_grid2), #5
 
       qrf7=caretModelSpec(method="xgbLinear", tuneGrid = final_grid3), #5
       qrf8=caretModelSpec(method="xgbLinear", tuneGrid = final_grid4) #5
 
       #qrf9=caretModelSpec(method="BstLm") #5
       #qrf8=caretModelSpec(method="cubist") #5
-      # qrf6=caretModelSpec(method="qrf", ntree=2, tuneLength = 1) #5
+     # qrf6=caretModelSpec(method="qrf", ntree=2, tuneLength = 1) #5
     )
     # ),
     # methodList = c(
@@ -170,32 +197,42 @@ xgblinearBV = function(){
 
   models.list2
 
-  # my_control <- trainControl(method = "boot", # for "cross-validation"
-  #                            number = 500, # number of k-folds
-  #                            #savePredictions = "final",
-  #                            allowParallel = TRUE, verbose=T)
-  #
-
   NCAA.stacked<-caretEnsemble(models.list2, # + 95
                               trControl = trainControl(
                                 number=2,
-                                method="cv",
+                                method="boot",
                                 verboseIter =TRUE,
                                 allowParallel = T
                               )
   );NCAA.stacked # + 95
 
   invisible(gc())
+  #626063 + 36001
+  #----Yield Val = 55
+  #----Yield tra = 66
+  #~~~~~~~~~~~~~~~~~~
+  #
+  #----pltht Val = 77
+  #----pltht tra = 86
+  #~~~~~~~~~~~~~~~~~~
+  #95958 + 5087
+  #----earht Val = 53
+  #----earht tra = 68
 
-  #######Validate Corn  52
+  #######Validate Corn
   preds = predict(NCAA.stacked, validatex2[,-1])
   cor(validatex2[, 1], preds)^2
   sqrt(mean((validatex2[, 1] -  preds)^2))
 
-  #######training set + 97  68
+  #######training set
   preds.t = predict(NCAA.stacked, trainx2[,-1])
   cor(trainx2[, 1], preds.t)^2
   sqrt(mean((trainx2[, 1] -  preds.t)^2))
+
+
+   # preds = data.frame(validatex2, round(preds,0))
+   # preds = left_join(preds, field[,-2], by=c("variety"="num"))
+   # preds = left_join(preds, field[,-2], by=c("round.preds..0."="num"))
 
   #######Soybeans + 99
   preds = predict(NCAA.stacked,  trainx1)
@@ -206,6 +243,9 @@ xgblinearBV = function(){
   preds = predict(NCAA.stacked,  validatex3)
   cor(data.frame(validatex3[,1]), (preds))^2
   sqrt(mean((validatex3[, 1] -  preds)^2))
+
+
+
 
 }
 
