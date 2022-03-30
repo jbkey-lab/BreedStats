48e3aa202ed104e2a9263b4866077abb1027cdfc
Jacob Lamkey <69020457+jbkey730@users.noreply.github.com>
2022-03-29 22:46:40 -0500

    file path updates

--- R/xgboostBV.R
+++ R/xgboostBV.R
@@ -61,53 +61,53 @@ xgblinearBV = function(  sdp,
 ){
 
   # #####################################################
-  s0=T
-  s1 =T
-  s2 =F
-  s3 =F
-  s4 =F
-  s5 =F
-  seas0 = 21
-  seas1 = 20
-  seas2 = ""
-  seas3 = ""
-  seas4 = ""
-  seas5 = ""
-  sdp = "C:/Users/jake.lamkey/Documents/"
-  fdp= "C:/Users/jake.lamkey/Documents/"
-  library(BreedStats)
-  library(tidyverse)
-  library(doParallel)
-  library(caretEnsemble)
-  library(caret)
-  library(data.table)
-  require(BGLR)
-  library(asreml)
+ # s0=T
+ # s1 =T
+#   s2 =F
+#   s3 =F
+#   s4 =F
+#   s5 =F
+#   seas0 = 21
+#   seas1 = 20
+#   seas2 = ""
+#   seas3 = ""
+#   seas4 = ""
+#   seas5 = ""
+#   sdp = "C:/Users/jake.lamkey/Documents/"
+#   fdp= "C:/Users/jake.lamkey/Documents/"
+#   library(BreedStats)
+#   library(tidyverse)
+#   library(doParallel)
+#   library(caretEnsemble)
+#   library(caret)
+#   library(data.table)
+#   require(BGLR)
+#   library(asreml)
 
-  season="21S"
-  rounds = 30
-  eta=1
-  alpha = .0003
-  lambda=.0003
-  male =   data.frame(male=c('BSQ033',	'GP734GTCBLL',	'BEX905',	'R08072HT',
-                             'BRQ529',	'8D2',	'SGI193',	'8SY',
-                             'GP718',	'FC2',	'BSR095',	'BRU059',
-                             'BRQ291',	'BRP251',	'TR6254RR2',	'BSQ033-PWRA',
-                             'BUR070',	'BRS312',	'8SY-AM',	'GP717Hx1',
-                             'BAC020',	'TPCJ6605',	'BSU151',	'SGI193-V2P',
-                             'BRQ064',	'GP6823Hx1',	'I10516',	'W8039RPGJZ',
-                             'FB6455',	'BSU311',	'GP717',	'BSQ002',
-                             'BAA441',	'GP738Hx1',	'BHH069',
-                             '84Z',	'TR4949',	'GP695Hx1',	'BSU313',
-                             'BHA493',	'R2846-NS6408DGV2P',	'I12003',	'R2846',
-                             'BSR273',	'BSQ941',	'BUR032',	'PRW-AM',
-                             'GP718Hx1',	'24AED-D02',"BAA419","BAA411","BHB075","BHJ471","GP702",
-                             "40QHQ-E07", "BQS941","BRS313","BSS009","GP738","BRR553",
-                             "BCA509","F8994","T1874","BUR011", "BQR334",
-                             "R6076","BRQ041","BBH030","F9898","85E","LFX7508","FC2YHR"
-  ))
-  genotype=F
-  seed = 30
+#   season="21S"
+#   rounds = 30
+#   eta=1
+#   alpha = .0003
+#   lambda=.0003
+#   male =   data.frame(male=c('BSQ033',	'GP734GTCBLL',	'BEX905',	'R08072HT',
+#                              'BRQ529',	'8D2',	'SGI193',	'8SY',
+#                              'GP718',	'FC2',	'BSR095',	'BRU059',
+#                              'BRQ291',	'BRP251',	'TR6254RR2',	'BSQ033-PWRA',
+#                              'BUR070',	'BRS312',	'8SY-AM',	'GP717Hx1',
+#                              'BAC020',	'TPCJ6605',	'BSU151',	'SGI193-V2P',
+#                              'BRQ064',	'GP6823Hx1',	'I10516',	'W8039RPGJZ',
+#                              'FB6455',	'BSU311',	'GP717',	'BSQ002',
+#                              'BAA441',	'GP738Hx1',	'BHH069',
+#                              '84Z',	'TR4949',	'GP695Hx1',	'BSU313',
+#                              'BHA493',	'R2846-NS6408DGV2P',	'I12003',	'R2846',
+#                              'BSR273',	'BSQ941',	'BUR032',	'PRW-AM',
+#                              'GP718Hx1',	'24AED-D02',"BAA419","BAA411","BHB075","BHJ471","GP702",
+#                              "40QHQ-E07", "BQS941","BRS313","BSS009","GP738","BRR553",
+#                              "BCA509","F8994","T1874","BUR011", "BQR334",
+#                              "R6076","BRQ041","BBH030","F9898","85E","LFX7508","FC2YHR"
+#   ))
+#   genotype=F
+#   seed = 30
 
   season0=as.numeric(seas0)
   season1=as.numeric(seas1)
@@ -603,7 +603,7 @@ xgblinearBV = function(  sdp,
                  refresh_leaf =0,
                  grow_policy ="lossguide",
                  max_bin = 10000,
-                 max_leaves =40,
+                 max_leaves =40
                  #sampling_method = "gradient_based"
                  #scale_pos_weight = 1
                  #subsample =.95
@@ -632,7 +632,7 @@ xgblinearBV = function(  sdp,
 
 
     #importance_matrix <- xgboost::xgb.importance(model = NCAA.stacked)
-    xgboost::xgb.save(bst, paste0(name,"_xgboost.model"))
+    xgboost::xgb.save(bst, paste0(fdp,name,"_xgboost.model"))
 
     # preds = predict(NCAA.stacked, validatex2 %>% dplyr::select(-feature) %>% as.matrix())
     # cat("r2 for Validate ALL is: ",cor(validatex2[, "feature"], preds)^2, "\n")
